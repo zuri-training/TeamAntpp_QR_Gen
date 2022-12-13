e50472b0c64793559c7a2b979777d71980d2a6d8
ruxy1212 <russelloje@gmail.com>
2022-12-14 00:01:58 +0100

    About us page incoming

--- public/assets/js/hscript.js
+++ public/assets/js/hscript.js
@@ -2,3 +2,12 @@ function menuOpen(){
     document.querySelector('.h-menu').classList.toggle('o-menu');
     document.querySelector('.h-nav-overflow').classList.toggle('unsee');
 }
+
+function profileDrop(){
+    document.querySelector('.dropdown-content').classList.toggle('unsee');
+}
+
+function mobileMenu(){
+    document.querySelector('.nav-links-parent').classList.toggle('unsee-mobile');
+    document.querySelector('.dropdown-content').classList.toggle('unsee-mobile');
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,161 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="{{ asset('assets/css/create.css') }}" />
+        <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/footer.css') }}"/>
+        <script type="text/javascript" src="{{ asset('assets/js/hscript.js') }}"></script>      
+        <title>User Dashboard - QRGo</title>
+    </head>
+    <body>
+        <header class="header">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="{{ asset('assets/images/transparent-logo1.svg') }}" alt="QR Go Logo" />
+                </div>
+                <nav class="nav-links-parent unsee-mobile">
+                    <li><a href="{{ route('home') }}">Home</a></li>
+                    <li><a href="{{ route('about') }}">About Us</a></li>
+                    <li><a href="{{ route('contact') }}">Contact Us</a></li>
+                </nav>
+                <div>
+                    <div class="profile">
+                        <img src="{{ asset('assets/images/profile-img.png') }}" alt="" />
+                        <div class="profile-details">
+                            <div class="profile-dropdown" onclick="profileDrop();">
+                                <h4>
+                                    <img
+                                        class="arrow-down"
+                                        src="{{ asset('assets/images/keyboard_arrow_down.png') }}"
+                                        alt=""
+                                    />
+                                    {{Auth::user()->name}}
+                                </h4>
+                            </div>
+                            <ul class="dropdown-content unsee unsee-mobile">
+                                <li class="dropdown">
+                                    <a href="#profile">Profile</a>
+                                </li>
+                                <li><a href="#create.html">Create QR</a></li>
+                                <li><a href="{{route('logout')}}" class="logout">Log out</a></li>
+                            </ul>
+                            <div class="profile-email">
+                                <p>{{Auth::user()->email}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img class="menubtn" src="{{ asset('assets/images/menu.svg') }}" alt="" onclick="mobileMenu();" />
+            </div>
+            <div class="white-line"></div>
+            <div class="blue-line"></div>
+        </header>
+        <main class="create-qr">
+            <aside class="sidebar">
+                <ul>
+                    <li class="sidebar-link activelink">
+                        <img src="{{ asset('assets/images/create.svg') }}" alt="" />Create
+                    </li>
+                    <li class="sidebar-link">
+                        <img src="{{ asset('assets/images/myqr.svg') }}" alt="" />My Qr
+                    </li>
+                    <li class="sidebar-link">
+                        <img src="{{ asset('assets/images/settings.svg') }}" alt="" />Settings
+                    </li>
+                </ul>
+            </aside>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+            <section class="create">
+                <div class="qr-method">
+                    <h1>Create QR for</h1>
+                    <div class="select-qr-method">
+                        <div class="url method">
+                            <img src="{{ asset('assets/images/bluelink.svg') }}" alt="" />
+                            <h2>URL</h2>
+                        </div>
+                        <div class="file method">
+                            <img src="{{ asset('assets/images/Vector.svg') }}" alt="" />
+                            <h2>File</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="recents">
+                    <h2>Recent</h2>
+                    <table>
+                        <thead>
+                            <tr class="table-heading">
+                                <th>Name</th>
+                                <th>Date</th>
+                                <th>Clicks</th>
+                            </tr>
+                        </thead>
+                        <tr>
+                            <td>
+                                <div>
+                                    <img
+                                        src="{{ asset('assets/images/link.svg') }}"
+                                        alt=""
+                                        class="method-img"
+                                    />
+                                </div>
+                                https://www.somefancyurl.com
+                            </td>
+                            <td>12-Nov-2022</td>
+                            <td>122</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div>
+                                    <img
+                                        class="method-img"
+                                        src="{{ asset('assets/images/attach_file.svg') }}"
+                                        alt=""
+                                    />
+                                </div>
+                                myselfie.jpg
+                            </td>
+                            <td>12-Nov-2022</td>
+                            <td>122</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div>
+                                    <img
+                                        src="{{ asset('assets/images/attach_file.svg') }}"
+                                        alt=""
+                                        class="method-img"
+                                    />
+                                </div>
+                                myselfie.jpg
+                            </td>
+                            <td>12-Nov-2022</td>
+                            <td>Not tracked</td>
+                        </tr>
+                    </table>
+                </div>
+            </section>
+        </main>
+        <footer class="footer">
+            <div class="footer-content">
+                <div class="logo">
+                    <img src="{{ asset('assets/images/white-qr-logo.svg') }}" alt="QR Go Logo">
+                </div>
+                <div class="footer-links">
+                    <li><a href="{{ route('home') }}" >Home</a></li>
+                    <li><a href="{{ route('about') }}">About Us</a></li>
+                    <li><a href="{{ route('contact') }}">Contact Us</a></li>
+                </div>
+                <div class="socials">
+                    <img src="{{ asset('assets/images/facebook-logo.svg') }}" alt="Facebook logo">
+                    <img src="{{ asset('assets/images/twitter-logo.svg') }}" alt="Twitter logo">
+                    <img src="{{ asset('assets/images/youtube-logo.svg') }}" alt="YouTube logo">
                 </div>
             </div>
-        </div>
-    </div>
-</x-app-layout>
+            <hr>
+            <p>Copyright@2022 Zuri Project Phase Team Ant. All Rights Reserved</p>
+        </footer>
+    </body>
+</html>
